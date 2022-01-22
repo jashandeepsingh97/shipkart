b8f1e6a4d63b144ff3c95122b17e69a95c27f7a1
jashan007 <jd38800@gmail.com>
2022-01-22 13:05:39 +0530

    backup

--- src/main/java/com/ecommerce/shipkart/config/MyDataRestConfig.java
+++ src/main/java/com/ecommerce/shipkart/config/MyDataRestConfig.java
@@ -1,5 +1,13 @@
 package com.ecommerce.shipkart.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -12,6 +20,13 @@ import com.ecommerce.shipkart.entity.ProductCategory;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+	private EntityManager entityManager;
+	
+	@Autowired
+	public MyDataRestConfig(EntityManager theEntityManager) {
+		entityManager=theEntityManager;
+	}
+	
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
@@ -27,5 +42,21 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
 				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
+		// call an internal helper method
+		exposeIds(config);
+	}
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+		// expose entity ids
+		
+		//get a list of all entity classes from entity manager
+		
+		Set<EntityType<?>> entities=entityManager.getMetamodel().getEntities();
+		
+		//create an array of entity types
+		List<Class> entityClasses =new ArrayList<>();
+		
+		//get the entity typs for the entities
+		
 	}
 }
